b5f2769fd5d3489c7dd04725ce724b85bb0e7d5f
Tatiana Minenko <ta.minenko@ya.ru>
2023-02-19 15:25:59 +0300

    users / basic User model

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "password"
+        "is_superuser",
+    )
+    list_editable = (
+        "password",
+    )
+    search_fields = (
+        "username",
+    )
