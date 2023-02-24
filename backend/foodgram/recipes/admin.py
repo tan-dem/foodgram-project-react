45d84a67bc9ec7e80d484570a803e71f28c7e005
Tatiana Minenko <ta.minenko@ya.ru>
2023-02-24 18:10:24 +0300

    recipes / models registration in Admin + isort

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,101 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingCart, Subscription, Tag)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "measurement_unit",
+    )
+    list_filter = ("name",)
+    search_fields = ("name",)
+    empty_value_display = "-empty-"
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "slug",
+    )
+    empty_value_display = "-empty-"
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "author",
+        "is_favorited",
+    )
+    list_filter = (
+        "author",
+        "name",
+        "tags",
+    )
+    empty_value_display = "-empty-"
+
+    def is_favorited(self, obj):
+        return f"Favorited {Favorite.objects.filter(recipe=obj).count()} times"
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "recipe",
+        "ingredient",
+    )
+    list_editable = (
+        "recipe",
+        "ingredient",
+    )
+    empty_value_display = "-empty-"
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "user",
+        "author",
+    )
+    list_editable = (
+        "user",
+        "author",
+    )
+    empty_value_display = "-empty-"
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "user",
+        "recipe",
+    )
+    list_editable = (
+        "user",
+        "recipe",
+    )
+    empty_value_display = "-empty-"
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "user",
+        "recipe",
+    )
+    list_editable = (
+        "user",
+        "recipe",
+    )
+    empty_value_display = "-empty-"
